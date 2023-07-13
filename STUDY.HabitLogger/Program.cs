68e2f729e69134663f728e3d0fc7585137ae6fc6
Cole Long <coleml16@gmail.com>
2023-07-13 17:22:44 -0500

    Added Delete and Update methods, and better data validation. Basic CRUD functionality working.

--- STUDY.HabitLogger/Program.cs
+++ STUDY.HabitLogger/Program.cs
@@ -8,13 +8,11 @@ namespace habit_logger
     class Program
     {
         static string connectionString = @"Data Source=habit-Logger.db";
-
         static void Main(string[] args)
         {
-            
-
             using (var connection = new SqliteConnection(connectionString))
             {
+
                 connection.Open();
                 var tableCmd = connection.CreateCommand();
 
@@ -28,6 +26,7 @@ namespace habit_logger
                 tableCmd.ExecuteNonQuery();
 
                 connection.Close();
+
             }
 
             GetUserInput();
@@ -46,15 +45,16 @@ namespace habit_logger
                 Console.WriteLine("Type 2 to Insert Record.");
                 Console.WriteLine("Type 3 to Delete Record.");
                 Console.WriteLine("Type 4 to Update Record.");
-                Console.WriteLine("---------------------------------------\n");
+                Console.WriteLine("------------------------------------------\n");
 
                 string command = Console.ReadLine();
 
-                switch(command)
+                switch (command)
                 {
                     case "0":
                         Console.WriteLine("\nGoodbye!\n");
                         closeApp = true;
+                        Environment.Exit(0);
                         break;
                     case "1":
                         GetAllRecords();
@@ -62,19 +62,23 @@ namespace habit_logger
                     case "2":
                         Insert();
                         break;
-                  //  case "3":
-                  //      Delete();
-                  //      break;
-                  //  case "4":
-                  //      Update();
-                  //      break;
+                    case "3":
+                        Delete();
+                        break;
+                    case "4":
+                        Update();
+                        break;
+                    default:
+                        Console.WriteLine("\nInvalid Command. Please type a number from 0 to 4.\n");
+                        break;
                 }
             }
+
         }
 
         private static void GetAllRecords()
         {
-            Console.Clear ();
+            Console.Clear();
             using (var connection = new SqliteConnection(connectionString))
             {
                 connection.Open();
@@ -86,17 +90,17 @@ namespace habit_logger
 
                 SqliteDataReader reader = tableCmd.ExecuteReader();
 
-                if(reader.HasRows)
+                if (reader.HasRows)
                 {
                     while (reader.Read())
                     {
                         tableData.Add(
                         new DrinkingWater
                         {
-                            Id = reader.GetInt32 (0),
+                            Id = reader.GetInt32(0),
                             Date = DateTime.ParseExact(reader.GetString(1), "dd-MM-yy", new CultureInfo("en-US")),
-                            Quantity = reader.GetInt32 (2)
-                        });
+                            Quantity = reader.GetInt32(2)
+                        }); ;
                     }
                 }
                 else
@@ -106,12 +110,12 @@ namespace habit_logger
 
                 connection.Close();
 
-                Console.WriteLine("-----------------------------------------\n");
-                foreach(var dw in tableData)
+                Console.WriteLine("------------------------------------------\n");
+                foreach (var dw in tableData)
                 {
                     Console.WriteLine($"{dw.Id} - {dw.Date.ToString("dd-MMM-yyyy")} - Quantity: {dw.Quantity}");
                 }
-                Console.WriteLine("-----------------------------------------\n");
+                Console.WriteLine("------------------------------------------\n");
             }
         }
 
@@ -119,15 +123,14 @@ namespace habit_logger
         {
             string date = GetDateInput();
 
-            int quantity = GetNumberInput("\n\nPlease insert number of glasses or other measure of your choice (no decimans allowed.)\n\n");
+            int quantity = GetNumberInput("\n\nPlease insert number of glasses or other measure of your choice (no decimals allowed)\n\n");
 
             using (var connection = new SqliteConnection(connectionString))
             {
                 connection.Open();
                 var tableCmd = connection.CreateCommand();
-
                 tableCmd.CommandText =
-                    $"INSERT INTO drinking_water(date, quantity) VALUES('{date}', {quantity})";
+                   $"INSERT INTO drinking_water(date, quantity) VALUES('{date}', {quantity})";
 
                 tableCmd.ExecuteNonQuery();
 
@@ -135,13 +138,84 @@ namespace habit_logger
             }
         }
 
+        private static void Delete()
+        {
+            Console.Clear();
+            GetAllRecords();
+
+            var recordId = GetNumberInput("\n\nPlease type the Id of the record you want to delete or type 0 to go back to Main Menu\n\n");
+
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+
+                tableCmd.CommandText = $"DELETE from drinking_water WHERE Id = '{recordId}'";
+
+                int rowCount = tableCmd.ExecuteNonQuery();
+
+                if (rowCount == 0)
+                {
+                    Console.WriteLine($"\n\nRecord with Id {recordId} doesn't exist. \n\n");
+                    Delete();
+                }
+
+            }
+
+            Console.WriteLine($"\n\nRecord with Id {recordId} was deleted. \n\n");
+
+            GetUserInput();
+        }
+
+        internal static void Update()
+        {
+            GetAllRecords();
+
+            var recordId = GetNumberInput("\n\nPlease type Id of the record would like to update. Type 0 to return to Main Menu.\n\n");
+
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+
+                var checkCmd = connection.CreateCommand();
+                checkCmd.CommandText = $"SELECT EXISTS(SELECT 1 FROM drinking_water WHERE Id = {recordId})";
+                int checkQuery = Convert.ToInt32(checkCmd.ExecuteScalar());
+
+                if (checkQuery == 0)
+                {
+                    Console.WriteLine($"\n\nRecord with Id {recordId} doesn't exist.\n\n");
+                    connection.Close();
+                    Update();
+                }
+
+                string date = GetDateInput();
+
+                int quantity = GetNumberInput("\n\nPlease insert number of glasses or other measure of your choice (no decimals allowed)\n\n");
+
+                var tableCmd = connection.CreateCommand();
+                tableCmd.CommandText = $"UPDATE drinking_water SET date = '{date}', quantity = {quantity} WHERE Id = {recordId}";
+
+                tableCmd.ExecuteNonQuery();
+
+                connection.Close();
+            }
+
+
+        }
+
         internal static string GetDateInput()
         {
-            Console.WriteLine("\n\nPlease insert the date: (Format: dd-mm-yy). Type 0 to return to main menu.");
+            Console.WriteLine("\n\nPlease insert the date: (Format: dd-mm-yy). Type 0 to return to Main Menu.\n\n");
 
             string dateInput = Console.ReadLine();
 
             if (dateInput == "0") GetUserInput();
+
+            while (!DateTime.TryParseExact(dateInput, "dd-MM-yy", new CultureInfo("en-US"), DateTimeStyles.None, out _))
+            {
+                Console.WriteLine("\n\nInvalid date. (Format: dd-mm-yy). Type 0 to return to Main Menu or try again:\n\n");
+                dateInput = Console.ReadLine();
+            }
 
             return dateInput;
         }
@@ -154,11 +228,18 @@ namespace habit_logger
 
             if (numberInput == "0") GetUserInput();
 
+            while (!Int32.TryParse(numberInput, out _) || Convert.ToInt32(numberInput) < 0)
+            {
+                Console.WriteLine("\n\nInvalid number. Try again.\n\n");
+                numberInput = Console.ReadLine();
+            }
+
             int finalInput = Convert.ToInt32(numberInput);
 
             return finalInput;
         }
     }
+
 
     public class DrinkingWater
     {
